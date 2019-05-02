01c5ba518b9693866ac3aef046b2ef9b70b271ec
Andrey <osokinpochta@gmail.com>
2019-05-02 18:46:25 -0400

    For Stephen

---
+++ Andrey/Player/app/src/main/java/com/andrey/player/Player.java
@@ -0,0 +1,18 @@
+package com.andrey.player;
+
+
+public class Player {
+
+    private int health;
+    String Player;
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int newHealth){
+        health = newHealth;
+    }
+
+
+}
